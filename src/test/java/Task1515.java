85a0dd73f6f83969828d0be4abab3f8a19ee7af3
Natalia <nataboyko37@gmail.com>
2021-05-11 18:52:00 -0700

    Add comments and handle for assertError

--- src/test/java/Task1515.java
+++ src/test/java/Task1515.java
@@ -21,31 +21,37 @@ public class Task1515 extends utils{
         WebElement NextButton=null;
         Boolean isEnd=true;
         List<WebElement> list=null;
-        while (isEnd) {
+        int NumberOfError=0;
+        //get list of result for every page
+          while (isEnd) {
             NextButton=driver.findElement(By.className("rc-pagination-next"));
             list = driver.findElements(By.xpath("//*[@id='product_listing']/div"));
             for (WebElement el : list) {
                 try {
+                    //check every item in the List result for word "Table"
                     Assert.assertTrue(el.findElement(By.className("description")).getText().toLowerCase().contains("table"));
                 } catch(AssertionError e) {
+                    NumberOfError++;
                     System.out.println(driver.getCurrentUrl() + "\n" + el.findElement(By.className("description")).getText());
                 }
             }
             if (NextButton.getAttribute("aria-disabled").equals("true")) isEnd = false;
             else NextButton.click();
         }
-
+        // add last item to the cart
        List<WebElement> listAddToCart = driver.findElements(By.xpath("//input[@type='submit']"));
         listAddToCart.get(listAddToCart.size()-1).click();
         JavascriptExecutor executor = (JavascriptExecutor)driver;
         executor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//a[@href='/viewcart.cfm']")));
+        if(NumberOfError>0)
+            Assert.fail("The test is failed\n" + "There are(is) "+ String.valueOf(NumberOfError)+" items, that doesn't contain 'table'\nSee the list in the console");
      }
-    @Test(priority = 3, dependsOnMethods = "CheckResultsAddToCart")
+    @Test(priority = 3)
     public void EmptyCart() throws InterruptedException {
+        //empty cart
         JavascriptExecutor executor = (JavascriptExecutor)driver;
         executor.executeScript("arguments[0].click();", driver.findElement(By.className("emptyCartButton")));
          executor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//*[@id='td']/div[6]/div/div/div/div[3]/button[1]")));
-
         Thread.sleep(1000);
         Assert.assertTrue(driver.findElement(By.xpath("//p[@class='header-1']")).getText().equals("Your cart is empty."));
    }
